bead51f7ce49a00055db95b7467f21c93c204b93
josue <josuensumba@gmail.com>
2023-07-21 21:52:29 +0200

    ...

--- src/main/java/org/cloud/api/UserController.java
+++ src/main/java/org/cloud/api/UserController.java
@@ -4,9 +4,8 @@ import org.cloud.dto.UserDto;
 import org.cloud.entity.User;
 import org.cloud.utility.EntityConverter;
 import org.cloud.repository.UserRepository;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,25 +14,44 @@ import java.util.stream.Collectors;
 @RequestMapping(path = "/users", produces = "application/json")
 public class UserController {
 
-    private UserRepository repository;
-    private EntityConverter converter = new EntityConverter();
+    private final UserRepository repository;
+    private final EntityConverter converter = new EntityConverter();
 
     UserController(UserRepository repository) {
         this.repository = repository;
     }
 
-    @RequestMapping("")
+    @GetMapping("")
     public List<UserDto> getUsers() {
-        List<UserDto> users  = repository.findAll()
+        return repository.findAll()
                 .stream()
                 .map(u -> converter.getUserDto(u))
                 .collect(Collectors.toList());
-        return users;
     }
 
-    @RequestMapping("/{id}")
+    @GetMapping("/{id}")
     public UserDto getUser(@PathVariable("id") Long id) {
+        return converter.getUserDto(repository.findById(id).get());
+    }
+
+    @PostMapping("")
+    public UserDto addUser(@Validated @RequestBody User user) {
+        return converter.getUserDto(repository.save(user));
+    }
+
+    @PutMapping("/{id}")
+    public UserDto updateUser(@PathVariable("id") Long id,
+                              @Validated @RequestBody User _user) {
         User user = repository.findById(id).get();
-        return converter.getUserDto(user);
+
+        user.setFirstName(_user.getFirstName());
+        user.setLastName(_user.getLastName());
+        user.setEmail(_user.getEmail());
+        return converter.getUserDto(repository.save(user));
+    }
+
+    @DeleteMapping("/{id}")
+    public void removeUser(@PathVariable("id") Long id) {
+        repository.deleteById(id);
     }
 }
